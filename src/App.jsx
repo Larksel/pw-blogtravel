ce5e634f66473942254693edd0c689d5222dadf8
Larksel <lemuelgames@outlook.com>
2024-04-10 14:27:53 -0300

    Aula dia 10/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import About from "./pages/About/About";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
@@ -7,12 +9,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
